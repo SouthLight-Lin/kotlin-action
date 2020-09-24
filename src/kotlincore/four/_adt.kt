2bc94456c08c6936f9796ae187d6ecef6b7db417
linnanwei <linnanwei@yy.com>
2020-09-24 09:58:20 +0800

    《Kotlin核心编程》：ADT和Seal class

--- src/kotlincore/four/_adt.kt
+++ src/kotlincore/four/_adt.kt
@@ -1,5 +1,7 @@
 package kotlincore.four
 
+import shangshou.scoping_function.User
+
 /**
  * 优惠券
  */
@@ -7,20 +9,52 @@ sealed class Coupon {
     companion object {
         // 代金券
         const val CashType = "CASH"
+
         // 折扣卷
         const val DiscountType = "DISCOUNT"
+
         // 礼品卷
         const val GiftType = "GIFT"
 
         const val NotFetched = 1    // 未领取
         const val Fetched = 2       // 已领取但未使用
-        const val Used  = 3         // 已使用
+        const val Used = 3         // 已使用
         const val Expire = 4        // 已过期
-        const val UnAvilable = 5     // 已失效
+        const val UnAvailable = 5     // 已失效
 
+        fun fetched(c: Coupon, user: User): Boolean = true
+        fun used(c: Coupon, user: User): Boolean = true
+        fun isExpired(c: Coupon): Boolean = true
+        fun isUnAvailable(c: Coupon): Boolean = true
+        fun getCouponStatus(coupon: Coupon, user: User): CouponStatus = when {
+            isUnAvailable(coupon) -> CouponStatus.StatusUnAvailable(coupon)
+            isExpired(coupon) -> CouponStatus.StatusExpired(coupon)
+            used(coupon, user) -> CouponStatus.StatusUsed(coupon, user)
+            fetched(coupon, user) -> CouponStatus.StatusFetched(coupon, user)
+            else -> CouponStatus.StatusNotFetched(coupon)
+        }
+
+        fun showStatus(status: CouponStatus) = when (status) {
+            is CouponStatus.StatusUnAvailable -> println("unavailable")
+            is CouponStatus.StatusExpired -> println("expire")
+            is CouponStatus.StatusUsed -> println("used")
+            is CouponStatus.StatusFetched -> println("fetched")
+            else -> println("not fetched")
+        }
     }
 
-    class CashCoupon(val id: Long, val type: String, val leastCost: Long, val reduceCost: Long): Coupon()
-    class DiscountCoupon(val id: Long, val type: String, val discount: Int): Coupon()
-    class GiftCoupon(val id: Long, val type: String, val gift: String): Coupon()
+    class CashCoupon(val id: Long, val type: String, val leastCost: Long, val reduceCost: Long) : Coupon()
+    class DiscountCoupon(val id: Long, val type: String, val discount: Int) : Coupon()
+    class GiftCoupon(val id: Long, val type: String, val gift: String) : Coupon()
+}
+
+/**
+ * 优惠券状态
+ */
+sealed class CouponStatus {
+    data class StatusNotFetched(val coupon: Coupon) : CouponStatus()
+    data class StatusFetched(val coupon: Coupon, val user: User) : CouponStatus()
+    data class StatusUsed(val coupon: Coupon, val user: User) : CouponStatus()
+    data class StatusExpired(val coupon: Coupon) : CouponStatus()
+    data class StatusUnAvailable(val coupon: Coupon) : CouponStatus()
 }
--- src/shangshou/scoping_function/main.kt
+++ src/shangshou/scoping_function/main.kt
@@ -13,6 +13,8 @@ package shangshou.scoping_function
 
 data class User(var name: String)
 
+data class Person(val name: String, val email: String)
+
 fun main(args: Array<String>) {
     var user = User("lnw")
 
@@ -27,14 +29,17 @@ fun main(args: Array<String>) {
     user.apply { println("apply::${this.javaClass}") }
 
     /** takeIf 的闭包返回一个判断结果，为false时，takeIf函数会返回空 **/
-    user.takeIf { it.name.isNotEmpty() }?.also { println("姓名为：${it.name}") }?:println("姓名为空")
+    user.takeIf { it.name.isNotEmpty() }?.also { println("姓名为：${it.name}") } ?: println("姓名为空")
     /** takeUnless 与 takeIf刚好相反，闭包的判断结果，为true时函数返回空 **/
-    user.takeUnless { it.name.isNotEmpty() }?.also { println("姓名为空") }?:println("姓名为：${user.name}")
+    user.takeUnless { it.name.isNotEmpty() }?.also { println("姓名为空") } ?: println("姓名为：${user.name}")
 
     /** with比较特殊，不是以扩展方法的形式存在，而是一个顶级函数 **/
-    with(user){
+    with(user) {
         this.name = "with"
     }
     // 其实相当于
     user.apply { this.name = "with" }
+
+    val (name, email) = Person("name", "email")
+
 }
